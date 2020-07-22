082546c4ebf8c9e792d10f24c951959f375ef559
Taha Elleuch <tahaeleuch@gmail.com>
2020-07-22 14:42:48 +0000

    0-binary_tree_node.c

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -10,9 +10,11 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	binary_tree_t *node;
 
 	node = malloc(sizeof(binary_tree_t));
-	if (node == NULL || parent == NULL)
+	if (node == NULL)
 		return (NULL);
 	node->parent = parent;
 	node->n = value;
+	new_node->left = NULL;
+	new_node->right = NULL;
 	return (node);
-} 
+}
