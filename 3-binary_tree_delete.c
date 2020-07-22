94d96bd050305e2b73925f8c7236594abcd13eba
Bzhamdi <bouzouitina.hamdi@gmail.com>
2020-07-22 19:38:10 +0000

    delete

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,13 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_delete - function that delete a binary_tree
+ * @tree: binary tree
+*/
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree == NULL)
+		exit(0);
+	binary_tree_delete(tree->left);
+	binary_tree_delete(tree->right);
+	free(tree);
+}
