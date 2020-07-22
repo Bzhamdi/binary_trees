d214d1cbd036877bf85f7870f7e34c54ca542918
Bzhamdi <bouzouitina.hamdi@gmail.com>
2020-07-22 19:10:55 +0000

    1

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,28 @@
+#include "binary_trees.h"
+
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *left;
+
+	if (parent == NULL)
+		return (NULL);
+	left = malloc(sizeof(binary_tree_t));
+	if (left == NULL)
+		return (NULL);
+	left->parent = parent;
+	left->n = value;
+	left->left = NULL;
+	left->right = NULL;
+	if (parent->left == NULL)
+	{
+		parent->left = left;
+	}
+	else
+	{
+		parent->left->parent = left;
+		left->left = parent->left;
+		parent->left = left;
+		return (left);
+	}
+	return (left);
+}
