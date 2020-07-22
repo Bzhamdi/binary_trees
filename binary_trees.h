03acce934ee23edb932d217f35971fabc3382e5f
Taha Elleuch <tahaeleuch@gmail.com>
2020-07-22 19:10:10 +0000

    7-binary_tree_inorder.c

--- binary_trees.h
+++ binary_trees.h
@@ -30,10 +30,8 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
 void binary_tree_delete(binary_tree_t *tree);
 int binary_tree_is_leaf(const binary_tree_t *node);
-<<<<<<< HEAD
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
-=======
 int binary_tree_is_root(const binary_tree_t *node);
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
 
->>>>>>> 7aeecfc322cfb6cb46e53c2698c06875d280c717
 #endif
