a49cdc99c43c66e9f504deb86a28dc5f6e538582
Bzhamdi <bouzouitina.hamdi@gmail.com>
2020-07-23 03:08:51 +0000

    13_tree_nodes

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_nodes- counts the leaves in a binary tree
+ * @tree: the tree
+ * Return: number
+*/
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+	if (tree->left == NULL && tree->right == NULL)
+		return (0);
+	else
+		return (binary_tree_nodes(tree->left) + 1 +
+		binary_tree_nodes(tree->right));
+}
+
